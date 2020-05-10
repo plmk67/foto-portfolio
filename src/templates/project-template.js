262a6044badeb6bc66ff6a50e4053c113b23cad8
Vincent <Yip.vincent@gmail.com>
2020-05-10 02:37:42 -0400

    adds navigation between images with cursor pointer directional icons on modal

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import styles from "./image-template.module.css"
 import get from "lodash.get"
 
-const Template = ({ location, data, pageContext }) => {
+const Template = ({ data, pageContext }) => {
   const { images } = get(data, pageContext.project)
 
   const [modal, setModal] = useState(false)
@@ -16,7 +16,27 @@ const Template = ({ location, data, pageContext }) => {
     setModal(!modal)
 
     if (isNaN(e.target.alt) === false) {
-      setIndex(e.target.alt)
+      setIndex(parseInt(e.target.alt))
+    }
+
+    console.log("current index " + index)
+  }
+
+  const previous = () => {
+    if (index <= 0) {
+      setIndex(0)
+      console.log("first image")
+    } else {
+      setIndex(index - 1)
+      console.log(index)
+    }
+  }
+
+  const next = () => {
+    if (index === images.length - 1) {
+      setIndex(images.length - 1)
+    } else {
+      setIndex(index + 1)
     }
   }
 
@@ -26,6 +46,11 @@ const Template = ({ location, data, pageContext }) => {
     display = (
       <Col className={styles.Layout_Modal}>
         <Row className={styles.Modal}>
+          <Row className={styles.Navigation}>
+            <Col onClick={previous} className={styles.Navigation__Previous} />
+            <Col onClick={toggle} className={styles.Navigation__Gallery} />
+            <Col onClick={next} className={styles.Navigation__Next} />
+          </Row>
           <Row onClick={toggle} className={styles.main_image}>
             <Img alt={images[index].id} fluid={images[index].fluid} />
           </Row>
